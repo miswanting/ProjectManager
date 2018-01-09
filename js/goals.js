4b5b47c66dd5c6e2b3b8b741ff2b0ae1600ba5cc
何雨航 <453542772@qq.com>
2018-01-09 14:59:05 +0800

    修复注释不完全问题中

--- js/goals.js
+++ js/goals.js
@@ -1,6 +1,6 @@
-var svg_goals = d3.select("#svg-goals")
-var btn_goals_add = d3.select("#btn-goals-add")
-var btn_goals_add_show = d3.select("#btn-goals-add-show")
+var svg_goals = d3.select("#svg-goals") // SVG元素的引用
+var btn_goals_add_show = d3.select("#btn-goals-add-show") // 新增节点页面显示按钮
+var btn_goals_add = d3.select("#btn-goals-add") // 新增节点确认按钮
 var width = 800,
     height = 600
 svg_goals.attr("width", width)
@@ -41,13 +41,15 @@ var stratify_goals = d3.stratify()
     }).parentId(function (d) {
         return d.parent;
     })(data);
+// 连线集合
 var link = svg_goals.append("g")
     .attr("class", "links")
     .attr("id", "goals-links")
+    // 整体偏移
     .attr("transform", function (d) {
         return "translate(" + 50 + "," + 0 + ")"
     })
-// .selectAll("g")
+// 每根连线
 link.selectAll("#goals-links")
     .data(tree(stratify_goals).links())
     .enter().append("path")
@@ -60,7 +62,8 @@ link.selectAll("#goals-links")
         }))
     .attr("stroke", "#000")
     .attr("stroke-width", 2)
-    .attr("fill", "none")
+    .attr("fill", "none") // 不填充
+// 节点集合
 var nodes = svg_goals.append("g")
     .attr("class", "nodes")
     .attr("id", "goals-nodes")
@@ -73,6 +76,7 @@ var nodes = svg_goals.append("g")
     .attr("transform", function (d) {
         return "translate(" + d.y + "," + d.x + ")"
     })
+// 圆点
 nodes.append("circle")
     .attr("r", 5)
     .attr("stroke", "#fff")
@@ -89,6 +93,7 @@ nodes.append("circle")
         d3.select("#goals-detail")
             .text(d3.select(this)._groups[0][0].__data__.id)
     })
+// 文本
 nodes.append("text")
     .attr("x", function (d) {
         return -6
@@ -99,6 +104,33 @@ nodes.append("text")
     .text(function (d) {
         return d.id.substring(d.id.lastIndexOf(".") + 1);
     })
+// 新增节点页面显示按钮
+btn_goals_add_show.on("click", function () {
+    d3.select("#goals-name")._groups[0][0].value = ""
+    stratify_goals = d3.stratify()
+        .id(function (d) {
+            return d.name;
+        }).parentId(function (d) {
+            return d.parent;
+        })(data)
+    var l_node = stratify_goals.descendants()
+    var l_id = []
+    for (var i = 0; i < l_node.length; i++) {
+        l_id.push(l_node[i].id)
+    }
+    console.log(l_id)
+    d3.select("#goals-parentlist")
+        .selectAll("option")
+        .remove()
+    d3.select("#goals-parentlist")
+        .selectAll("option")
+        .data(l_id)
+        .enter().append("option")
+        .text(function (d) {
+            return d
+        })
+})
+// 新增节点确认按钮
 btn_goals_add.on("click", function () {
     if (d3.select("#goals-name")._groups[0][0].value == "") {
         d3.select("#goals-name")._groups[0][0].value = "[NONE]"
@@ -175,30 +207,5 @@ btn_goals_add.on("click", function () {
         .data(stratify_goals.descendants())
         .text(function (d) {
             return d.id.substring(d.id.lastIndexOf(".") + 1);
-        })
-})
-btn_goals_add_show.on("click", function () {
-    d3.select("#goals-name")._groups[0][0].value = ""
-    stratify_goals = d3.stratify()
-        .id(function (d) {
-            return d.name;
-        }).parentId(function (d) {
-            return d.parent;
-        })(data)
-    var l_node = stratify_goals.descendants()
-    var l_id = []
-    for (var i = 0; i < l_node.length; i++) {
-        l_id.push(l_node[i].id)
-    }
-    console.log(l_id)
-    d3.select("#goals-parentlist")
-        .selectAll("option")
-        .remove()
-    d3.select("#goals-parentlist")
-        .selectAll("option")
-        .data(l_id)
-        .enter().append("option")
-        .text(function (d) {
-            return d
         })
 })
